1f8adb2280c70eeac7b53c558bbfec263b0a705a
Cotes <cotes.brandon@yahoo.com>
2018-12-21 19:36:59 -0500

    reset button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,13 @@
 // on window load 
-window.onload = function() {
+window.onload = function begin() {
   $(".first").show()
   $(".game").hide()
   $(".end").hide()
   $(".start").on("click", start);
   $("#timer").text("Time left: " + time);
   $(".submit").on("click", endGame);
+  $(".reset").on("click", reset);
   clockRunning = false;
-  $(".question1").text("What color is the sky?");
-  $(".question2").text("What color is the grass?");
-  $(".question3").text("How many days are in a year?");
-  $(".question4").text("What's the shortest month of the year?");
-  $(".question5").text("what is 5-2?")
 }
 
 // variables
@@ -20,7 +16,7 @@ var time = 30;
 var clockRunning = false;
 var correctAnswers = 0;
 var wrongAnswers = 0;
-var gameActive = false;
+
 
 
 // start button function
@@ -35,59 +31,68 @@ function start() {
 }
 
 // timer function
- function decrement() {
-   time--;
-   $("#timer").text("Time left: " + time);
-   if (time === 0) {
-     endGame();
-   }
- }
+function decrement() {
+  time--;
+  $("#timer").text("Time left: " + time);
+  if (time === 0) {
+    endGame();
+  }
+}
 
-// stops timer
- function endGame () {
-   clearInterval(intervalId);
+// end game function stops timer, runs players choices against correct answers, prints correct and wrong answers to final screen
+function endGame() {
+  clearInterval(intervalId);
+// loop goes through players answers
+  for (let i = 0; i < answers.length; i++) {
+    if ($(`input[name="Q${i}"]:checked`).val() === answers[i].correctAnswers) {
+      correctAnswers++;
+    } else {
+      wrongAnswers++;
+    }
+  }
+  $(".start").hide()
+  $(".game").hide()
+  $(".end").show()
+  $(".correct").text("Correct Answers: " + correctAnswers);
+  $(".wrong").text("Wrong Answers: " + wrongAnswers)
+}
 
-   for (let i = 0; i < answers.length; i++) {
-     
-     if ($(`input[name="Q${i+1}"]:checked`).val() === answers[i].correctAnswers) {
-       correctAnswers++;
-       }
-       else {
-         wrongAnswers++;
-       }
-   }
-   $(".start").hide()
-   $(".game").hide()
-   $(".end").show()
-   $(".correct").text("Correct Answers: " + correctAnswers);
-   $(".wrong").text("Wrong Answers: " + wrongAnswers)
- }
-
+// answers array
 var answers = [{
-  answers:  ["Red", "Blue", "Green", "Yellow"],
-  correctAnswers: "Blue"},
-  
-    {answers: ["Red", "Blue", "Green", "Yellow"],
-    correctAnswers: "Green"},
-    {
-      answers: ["600", "1250", "1738", "365"],
-      correctAnswers: "365"},
-      {
-        answers: ["January", "December", "July", "February"],
-        correctAnswers: "February"
-      },
-      {
-        answers: ["0", "1", "2", "-3"],
-        correctAnswers: "2"
-      }];
+    answers: ["Red", "Blue", "Green", "Yellow"],
+    correctAnswers: "Blue"
+  },
 
-   
+  {
+    answers: ["Red", "Blue", "Green", "Yellow"],
+    correctAnswers: "Green"
+  },
+  {
+    answers: ["600", "1250", "1738", "365"],
+    correctAnswers: "365"
+  },
+  {
+    answers: ["January", "December", "July", "February"],
+    correctAnswers: "February"
+  },
+  {
+    answers: ["0", "1", "3", "-2"],
+    correctAnswers: "3"
+  }
+];
 
-
-  
-
-
-
-
-
- 
+// reset function
+// reset "works" but when reset previous answers still displayed and after first game correct/wrong doesn't add up correctly
+function reset() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+  $(".first").hide()
+  $(".game").show()
+  $(".end").hide()
+  $(".start").on("click", start);
+  $("#timer").text("Time left: " + time);
+  $(".submit").on("click", endGame);
+  clockRunning = true;
+  wrongAnswers = 0;
+  correctAnswers = 0;
+}
